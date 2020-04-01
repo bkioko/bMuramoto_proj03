58e95ce59967375bbf26481c9c04cdcb3f4d47bf
bkioko <59579466+bkioko@users.noreply.github.com>
2020-04-01 12:00:17 -0700

    Player, Movement and Jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 25f;
+
+    float horizontalMove = 0f;
+    bool jumpFlag = false;
+    bool jump = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (jumpFlag)
+        {
+            jumpFlag = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    public void OnLanding()
+    {
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
+    }
+}
